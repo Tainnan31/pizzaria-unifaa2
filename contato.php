2f457732652c2073caa3e82e727ee4d2d56a2dbe
Tainnan31 <134459552+Tainnan31@users.noreply.github.com>
2024-06-27 15:23:54 -0300

    Create contato.php

---
+++ contato.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contato - Pizzaria UNIFAA</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <?php include 'menu.php'; ?>
+    <h1>Contato</h1>
+    <p>Email: contato@pizzariaunifaa.com.br</p>
+    <p>Telefone: (21) 1234-5678</p>
+</body>
+</html>
