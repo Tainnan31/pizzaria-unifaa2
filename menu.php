ff195a9ee199249463d268676879f88363931396
Tainnan31 <134459552+Tainnan31@users.noreply.github.com>
2024-06-27 15:23:12 -0300

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,7 @@
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="cardapio.php">Cardápio</a></li>
+        <li><a href="contato.php">Contato</a></li>
+    </ul>
+</nav>
