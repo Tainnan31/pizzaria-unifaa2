65b74f899d1394f66c693021330e401dbc6e8a3b
Tainnan31 <134459552+Tainnan31@users.noreply.github.com>
2024-06-27 15:23:33 -0300

    Create cardapio.php

---
+++ cardapio.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cardápio - Pizzaria UNIFAA</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <?php include 'menu.php'; ?>
+    <h1>Cardápio</h1>
+    <ul>
+        <li>Pizza Margherita - R$ 25,00</li>
+        <li>Pizza Calabresa - R$ 30,00</li>
+        <li>Pizza Quatro Queijos - R$ 35,00</li>
+        <li>Pizza Portuguesa - R$ 28,00</li>
+    </ul>
+</body>
+</html>
