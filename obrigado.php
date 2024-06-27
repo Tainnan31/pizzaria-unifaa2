c634355858ed024a52f4de4311b79226cf7f52a9
Tainnan31 <134459552+Tainnan31@users.noreply.github.com>
2024-06-27 15:25:53 -0300

    Create obrigado.php

---
+++ obrigado.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Obrigado - Pizzaria UNIFAA</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <?php include 'menu.php'; ?>
+    <h1>Obrigado pelo seu pedido!</h1>
+    <p>Seu pedido foi realizado com sucesso. Em breve você receberá a confirmação.</p>
+</body>
+</html>
